0acd6297129a5637dd57399935609b27a888c31f
paohuz <paohuz@gmail.com>
2019-11-01 18:59:51 +0900

    change log path

--- log.py
+++ log.py
@@ -10,7 +10,7 @@ class Logger(Enum):
 
 class Log:
     def __init__(self):
-        logging.basicConfig(level=logging.INFO, filename='pg.log',
+        logging.basicConfig(level=logging.INFO, filename='/kw_resources/pg.log',
                             filemode='w', format='%(asctime)s-%(process)d-%(name)s\n%(levelname)s\n%(message)s')
 
         self.feature_norm = logging.getLogger(str(Logger.feature_norm))
